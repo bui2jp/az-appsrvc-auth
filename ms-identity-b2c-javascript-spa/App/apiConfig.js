8000dba2a70799406f0981eb7699af1f0579278d
takuya <>
2023-01-30 08:54:23 +0900

    update

---
+++ ms-identity-b2c-javascript-spa/App/apiConfig.js
@@ -0,0 +1,8 @@
+// The current application coordinates were pre-registered in a B2C tenant.
+const apiConfig = {
+    //
+    // b2cScopes: ["https://tcase2023org.onmicrosoft.com/demo/demo.read"],
+    b2cScopes: ["https://tcase2023org.onmicrosoft.com/spa-demo/test"],
+    //b2cScopes: ["offline_access"],
+    webApi: "https://myappservice-25595.azurewebsites.net/users"
+  };
